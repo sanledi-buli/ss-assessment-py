03ec0592b3c74f30e21684dd001962244295a3ba
sanledi buli <sanledi.buli@gmail.com>
2016-02-26 00:01:45 +0700

    Refactor: using phone as key for dictionary

--- task-3.py
+++ task-3.py
@@ -1,5 +1,6 @@
 import pdb
 from werkzeug.wrappers import Request, Response
+from werkzeug.serving import run_simple
 
 arr_dict = []
 
@@ -7,13 +8,18 @@ def initialize(blacklist):
     with open(blacklist) as f:
         for line in f:
             cols = [x for x in line.split(' ')]
-            arr_dict.append({'name' : cols[0], 'phone' : cols[1].replace('\n','')})
+            arr_dict.append({ cols[1].replace('\n','') : cols[0] })
 
 """
 I think phone number for every user is unique. But lets check the name too.
 """
 def check_blacklist(name, phone):
-    return any(d['phone'] == phone and d['name'] == name for d in arr_dict)
+    item = None
+    try:
+        item = next((item for item in arr_dict if item[phone] == name), None)
+    except KeyError, e:
+        pass
+    return True if item is not None else False
 
 @Request.application
 def application(request):
@@ -23,11 +29,10 @@ def application(request):
         phone = request.args.get('phone')
         if phone is not None and name is not None:
             return Response("%r" % check_blacklist(name, phone))
-        return Response("False", status=200)
+        return Response("Bad Request", status=400)
     return Response("Not Found", status=404)
 
 
 if __name__ == '__main__':
-    from werkzeug.serving import run_simple
     initialize('files/blacklist.txt')
     run_simple('127.0.0.1', 3000, application)
