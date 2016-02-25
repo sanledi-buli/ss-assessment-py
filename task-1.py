7e8601d5b68f585f01b80aa7373693ad943a3860
sanledi buli <sanledi.buli@gmail.com>
2016-02-25 21:13:11 +0700

    Refactor: task-1.py

--- task-1.py
+++ task-1.py
@@ -1,12 +1,15 @@
 import sys
 import pdb
 
+"""
+Result for sys.getsizeof(arr_ages) is 80MB. I think its safe to use this mechanism.
+
+"""
+
 arr_ages = []
 with open('files/ages-10-million.txt') as f:
     for line in f:
-        elems = line.split(',')
-        elem = int(elems[0].replace('\n',""))
-        arr_ages.append(elem)
+        arr_ages.append(int(line.replace('\n','')))
 
 sorted_age = open("files/ages-10-million-shorted.txt","w")
 
